e964ee92eef178186971aa608b73f0258a6e2492
AdhamUsama25 <95590437+AdhamUsama25@users.noreply.github.com>
2021-12-09 20:43:50 +0200

    Update Clinic.py

--- Clinic.py
+++ Clinic.py
@@ -21,26 +21,6 @@ class Queue:
         return len(self.items)
 
 
-class Doctor:
-    def __init__(self, patients):
-        self.treating_rate = patients
-        self.current_patient = None
-        self.time_remaining = 0
-
-    def tick(self):
-        if self.current_patient is not None:
-            self.time_remaining -= 1
-            if self.time_remaining == 0:
-                self.current_patient = None
-
-    def busy(self):
-        return self.current_patient is not None
-
-    def enter_next(self, new_patient):
-        self.current_patient = new_patient
-        self.time_remaining = new_patient.get_age()/self.treating_rate
-
-
 class Patient:
     def __init__(self, time):
         self.timestamp = time
@@ -52,16 +32,48 @@ class Patient:
     def waiting_time(self, current_time):
         return current_time - self.timestamp
 
+
+class Doctor:
+    def __init__(self, patients):
+        self.treating_rate = patients
+        self.current_patient = None
+        self.time_remaining = 0
+
+    def tick(self):
+        if self.current_patient is not None:
+            self.time_remaining -= 1
+            if round(self.time_remaining) == 0:
+                self.current_patient = None
+
+    def busy(self):
+        return self.current_patient is not None
+
+    def enter_next(self, new_patient):
+        self.current_patient = new_patient
+        self.time_remaining = round(new_patient.get_age()/self.treating_rate) * 60
+
+
+def time_format(a_time):
+    h = a_time // 3600
+    m = (a_time - h * 3600) // 60
+    s = a_time - h * 3600 - m * 60
+    if a_time >= 3600:
+        return f'{int(h)} hrs, {int(m)} mins, {str("{:.2f}".format(s))} secs'
+    elif a_time >= 60:
+        return f'{int(m)} mins, {str("{:.2f}".format(s))} secs'
+    else:
+        return f'{str("{:.2f}".format(s))} secs'
+
 # Project:
 
 
-def simulation(treating_rate):
-    doctor_x = Doctor(treating_rate)
+def simulation(num_of_hours, treating_factor):
+    doctor_x = Doctor(treating_factor)
     patients_queue = Queue()
     waiting_times = []
 
-    for current_second in range(4 * 3600):
-        if random.randrange(1, 361) == 360:
+    for current_second in range(num_of_hours * 3600):
+        if random.randrange(1, 361) == 360:     # The rate of the clinic is 10 patients per hour. 10/3600 = 360
             patient = Patient(current_second)
             patients_queue.enqueue(patient)
 
@@ -73,8 +85,8 @@ def simulation(treating_rate):
         doctor_x.tick()
 
     average_wait = sum(waiting_times)/len(waiting_times)
-    print("Average Wait", str("{:.2f}".format(average_wait)), "secs", patients_queue.size(), "patients remaining")
+    print("Average Wait", time_format(average_wait), "and", patients_queue.size(), "patients remaining")
 
 
 for i in range(10):
-    simulation(10)
+    simulation(4, 10)
